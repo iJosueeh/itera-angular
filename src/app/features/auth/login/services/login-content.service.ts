41aae16347a0d386c10b1934ab1d6cf623aacbbc
IJosueeh <royer.tanta27@gmail.com>
2026-06-08 12:33:50 -0500

    feat: configure proxy and http client for local scala backend connectivity

--- src/app/features/auth/login/services/login-content.service.ts
+++ src/app/features/auth/login/services/login-content.service.ts
@@ -2,14 +2,14 @@ import { inject, Injectable, signal } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AuthResult, LoginPayload, LoginViewModel } from '@features/auth/interfaces/auth.interface';
 import { LOGIN_VIEW_MODEL_MOCK } from '../mocks/login.mock';
-import { AuthApiMockService } from '@features/auth/services/auth-api.mock.service';
+import { AuthApiService } from '@features/auth/services/auth-api.service';
 
 @Injectable({ providedIn: 'root' })
 export class LoginContentService {
-  private readonly authApiMockService = inject(AuthApiMockService);
+  private readonly authApiService = inject(AuthApiService);
   readonly vm = signal<LoginViewModel>(LOGIN_VIEW_MODEL_MOCK);
 
   signIn(payload: LoginPayload): Observable<AuthResult> {
-    return this.authApiMockService.signIn(payload);
+    return this.authApiService.signIn(payload);
   }
 }
